05e57f7cceb8687ca56dee5ef3ab1b8e8c8e11fb
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-17 17:41:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "ecs_cluster_id" {
-  description = "The ID of the ECS cluster"
-  value       = module.ecs_cluster.ecs_cluster_id
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = module.ecs_cluster.ecs_cluster_name
 }
 
 output "app_service_name" {
