d3d54fec72f2f352ba9b44549a9e90846e08a9a5
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-17 10:59:55 +0530

    vars

--- main.tf
+++ main.tf
@@ -24,27 +24,20 @@ module "iam" {
 }
 
 module "ecs_cluster" {
-  source = "./modules/ecs"
+  source       = "./modules/ecs"
+  cluster_name = "my-ecs-cluster"
 
-  cluster_name       = "my-ecs-cluster"
   nodejs_image       = var.nodejs_image
   mongodb_image      = var.mongodb_image
   instance_type      = var.instance_type
-
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
-
   vpc_id             = module.network.vpc_id
-  subnet_ids         = [
-    module.network.subnet_public_1_id,
-    module.network.subnet_public_2_id
-  ]
+  subnet_ids         = [module.network.subnet_public_1_id, module.network.subnet_public_2_id]
   security_group_id  = module.network.ecs_instance_sg_id
-
   execution_role_arn = module.iam.execution_role_arn
   task_role_arn      = module.iam.task_role_arn
-
   key_name           = var.key_name
   region             = var.region
 }
