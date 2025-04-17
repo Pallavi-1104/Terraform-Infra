2da6ef502cdc1ce9c4294f7f2bb865a06bf823e0
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-17 17:48:15 +0530

    Update userdata.sh

--- modules/ecs/userdata.sh
+++ modules/ecs/userdata.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-echo ECS_CLUSTER=${cluster_name} >> /etc/ecs/ecs.config
-
+echo "ECS_CLUSTER=my-ecs-cluster" >> /etc/ecs/ecs.config
 yum update -y
-yum install -y docker ecs-init
-
+yum install -y docker
+service docker start
 systemctl enable docker
-systemctl start docker
-
-systemctl enable ecs
-systemctl start ecs
-
+yum install -y ecs-init
+systemctl enable --now ecs
 mkdir -p /ecs/mongo-data
 mkdir -p /ecs/prometheus
